fea877cd5a8160077e8900c35c851947a8dd4049
Alexander Greff <alexander@greffsite.com>
2025-07-15 15:34:57 -0400

    First project using Hardware Manager  to Urbana board, configured switches -> LEDs

--- first_board_project/first_board_project.srcs/sources_1/new/buff.v
+++ first_board_project/first_board_project.srcs/sources_1/new/buff.v
@@ -21,9 +21,15 @@
 
 
 module buff(
-    input I,
-    output O
+    input in_0,
+    input in_1,
+    input [8:3] I,
+    output out_0,
+    output out_1,
+    output [8:3] O
     );
-    assign O = I;
+    assign out_0 = in_0;
+    assign out_1 = in_1;   
+    assign O = I; 
 
 endmodule
