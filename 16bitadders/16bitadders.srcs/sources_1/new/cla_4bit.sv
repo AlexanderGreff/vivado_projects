68e8e66a4e871b61da2e7cccdcfa0b98e463b118
Alexander Greff <alexander@greffsite.com>
2025-09-14 21:23:29 -0500

    top level, commented out rca. Fixed inner sum of CLA

--- 16bitadders/16bitadders.srcs/sources_1/new/cla_4bit.sv
+++ 16bitadders/16bitadders.srcs/sources_1/new/cla_4bit.sv
@@ -22,7 +22,7 @@
 
 module cla_4bit
     (
-     input  logic [3:0] a_in, 
+    input  logic [3:0] a_in, 
     input  logic  [3:0] b_in,
 	input  logic  cin,
 	output logic  pbit,
@@ -32,16 +32,23 @@ module cla_4bit
     
 logic  [3:0] p;
 logic  [3:0] g;
+logic [3:0] cbit;
 
 genvar i;
 for (i = 0; i < 4; i = i + 1)
 begin
 	assign p[i] = a_in[i] ^ b_in[i];
 	assign g[i] = a_in[i] & b_in[i];
-	assign s_out[i] = a_in[i] ^ b_in[i] ^ cin;
 end	
+
+    assign cbit[0] = cin;
+    assign cbit[1] = g[0] | (p[0] & g[0]);
+    assign cbit[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & cbit[0]);
+    assign cbit[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & cbit[0]);
 
 	assign pbit = &p;
 	assign gbit = g[3] | (g[2] & p[3]) | (g[1] & p[3] & p[2]) | (g[0] & p[3] & p[2] & p[1]);
+	
+	assign s_out = p ^ cbit;
 
 endmodule
